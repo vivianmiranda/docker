31ff86ed86dda8cb125b29938ed20e157d898f6d
Vivian <vivian.miranda.astro@gmail.com>
2024-07-19 01:04:05 -0400

    Update

---
+++ cocoa/init_cocoa39.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+ln -s "${CONDA_DIR}"/envs/cocoa/bin/x86_64-conda_cos6-linux-gnu-gcc "${CONDA_DIR}"/envs/cocoa/bin/gcc
+ln -s "${CONDA_DIR}"/envs/cocoa/bin/x86_64-conda_cos6-linux-gnu-g++ "${CONDA_DIR}"/envs/cocoa/bin/g++
+ln -s "${CONDA_DIR}"/envs/cocoa/bin/x86_64-conda_cos6-linux-gnu-gfortran "${CONDA_DIR}"/envs/cocoa/bin/gfortran
+ln -s "${CONDA_DIR}"/envs/cocoa/bin/x86_64-conda-linux-gnu-gcc-ar "${CONDA_DIR}"/envs/cocoa/bin/gcc-ar
+ln -s "${CONDA_DIR}"/envs/cocoa/bin/x86_64-conda-linux-gnu-gcc-ranlib "${CONDA_DIR}"/envs/cocoa/bin/gcc-ranlib
